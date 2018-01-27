f6f966c6b5ad991a32600c25d4c457feb0afebc0
jamescross91 <james_cross91@hotmail.com>
2018-01-27 22:05:49 +0000

    Done!

---
+++ lambda_function.py
@@ -0,0 +1,40 @@
+from ring_doorbell import Ring
+import os
+import boto3
+
+username = os.environ["USER"]
+password = os.environ["PASS"]
+
+dynamodb = boto3.resource(
+    'dynamodb',
+    region_name='eu-west-1'
+)
+
+table = dynamodb.Table('ring-device-events')
+
+
+def lambda_handler(event, context):
+    myring = Ring(username, password)
+
+    push_to_dynamo(myring.doorbells)
+    push_to_dynamo(myring.stickup_cams)
+
+
+def push_to_dynamo(devices):
+    print("Writing event history for " + str(devices))
+    for device in devices:
+        for event in device.history(limit=25):
+            dynamo_event = {
+                "device": device.id,
+                "device_name": device.name,
+                "device_connection_strength": device.wifi_signal_category,
+                "device_category": device.family,
+                "event_timestamp": event["created_at"].isoformat(),
+                "event_type": event["kind"],
+                "event_id": event["id"]
+            }
+
+            table.put_item(Item=dynamo_event)
+
+
+lambda_handler(None, None)
